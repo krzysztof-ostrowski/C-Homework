c84d62301ac6c6a1ba0e1aa03ab55f281a202301
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-12-21 21:30:31 +0100

    First Homework added

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+    using System.Xml;
+
+    string[] input = new[] {"dog", "cat", "redivider", "kayak", "tea", "reviver"};
+
+    var palindrome = PalindromeFinder(input);
+    foreach (var item in palindrome)
+    {
+        Console.WriteLine(item.ToString());
+    }
+
+
+
+static string[] PalindromeFinder(string[] input)
+{
+    List<string> result = new List<string>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char[] toChars = input[i].ToCharArray();
+            char[] toCharsReversed = input[i].ToCharArray();
+            Array.Reverse(toCharsReversed);
+            bool isEqual = Enumerable.SequenceEqual(toChars, toCharsReversed);
+            if (isEqual)
+            {
+                result.Add(input[i]);
+            }
+
+            
+
+
+        }
+
+        return result.ToArray();
+    }
+    
+
+    
---
+++ Homework1/Homework1a/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+
+string[] input = new[] { "dog", "cat", "redivider", "kayak", "tea", "reviver" };
+var palindrome = PalindromeFinder(input);
+foreach (var item in palindrome)
+{
+    Console.WriteLine(item.ToString());
+}
+
+static string[] PalindromeFinder(string[] input)
+
+{
+    List<string> result = new List<string>();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        char[] toChars = input[i].ToCharArray();
+        char[] toCharsReversed = new char[toChars.Length];
+        for (int j = toChars.Length - 1; j >= 0; j--)
+        {
+            toCharsReversed[toChars.Length - j - 1] = toChars[j];
+        }
+
+        bool isEqual = true;
+        for (int j = 0; j < toChars.Length; j++)
+        {
+            if (toChars[j] == toCharsReversed[j])
+                isEqual = true;
+            else
+            {
+                isEqual = false;
+                break;
+            }
+        }
+        if (isEqual)
+        {
+            result.Add(input[i]);
+        }
+    }
+    return result.ToArray();
+}
+
+
+
+
+
+
+
+    
+
+    
+
