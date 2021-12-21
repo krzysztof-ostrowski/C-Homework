c5150d6a2ba77490f0134b89322432e6acca0342
Krzysztof Ostrowski <ostrowski021@gmail.com>
2021-12-21 23:18:12 +0100

    Homework2 added

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+int[] array1 = new int[] {1, 2, 3, 4, 5};
+int[] array2 = new int[] {1, 2, 6, 7, 8};
+var result=ArrayAdding(array1, array2);
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+    
+}
+
+static int[] ArrayAdding(int[] input1, int[] input2)
+{
+    int j = 0;
+    List<int> result = new List<int>();
+    for (int i = 0; i < input1.Length; i++)
+    {
+        
+        if (input1[i] == input2[i])
+        {
+            result.Add(input1[i]);
+            //result[j] = input1[i];
+            //j++;
+        }
+        else
+        {
+            result.Add(input1[i]);
+            result.Add(input2[i]);
+            //result[j] = input1[i];
+            //result[j+1]=input2[i];
+            //j += 2;
+        }
+    }
+
+    for (int i = 0; i < result.Count; i++)
+    {
+        if (result[i]==0)
+        {
+            result.Remove(i);
+        }
+    }
+
+    
+    
+  
+    return result.ToArray();
+}
